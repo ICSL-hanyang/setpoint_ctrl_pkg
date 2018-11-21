944195647934a95cdd1138b54b2caa31102d66b1
Doolly <treeland88@hotmail.com>
2018-11-21 22:02:14 +0900

    s

--- src/swarm_algorithm.cpp
+++ src/swarm_algorithm.cpp
@@ -5,10 +5,11 @@
 #include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <tf2_ros/Vector3.h>
 
 tf2_ros::Buffer tfBuffer;
 tf2_ros::TransformListener tfListener(tfBuffer);
-void getNeighbors() // 
+void getNeighbors() //
 {
     geometry_msgs::TransformStamped transformStamped;
 
@@ -19,6 +20,9 @@ void getNeighbors() //
             transformStamped = tfBuffer.lookupTransform("camila@_base_link", "camila@_base_link",
                                                         ros::Time(0));
             //벡터로 바꿔서 전역 벡터 배열에 넣기
+            tf::Vector3 vect_1 (0,0,0);
+            vect_1 = transformStamped.getOrigin();
+            float dist_vect_1 = vect_1.distance(vect_1);
         }
         catch (tf2::TransformException &ex)
         {
@@ -29,6 +33,5 @@ void getNeighbors() //
     }
 }
 
-Vector3 seek(){}
-Vector3 seperate(){}
-
+Vector3 seek() {}
+Vector3 seperate() {}
