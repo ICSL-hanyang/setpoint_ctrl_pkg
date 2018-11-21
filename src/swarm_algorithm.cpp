d1e29b2810ff25cafd13915fea962f4f3d273ef8
Doolly <treeland88@hotmail.com>
2018-11-21 21:38:17 +0900

    병쉰같은 요구사항

---
+++ src/swarm_algorithm.cpp
@@ -0,0 +1,34 @@
+//연산자들 - http://docs.ros.org/latest/api/tf2/html/Vector3_8h_source.html#l00361
+//http://docs.ros.org/latest/api/tf2/html/namespacetf2.html#a49192866d5c81a02cc25119c27d0a401
+//http://wiki.ros.org/tf2
+
+#include <ros/ros.h>
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+
+tf2_ros::Buffer tfBuffer;
+tf2_ros::TransformListener tfListener(tfBuffer);
+void getNeighbors() // 
+{
+    geometry_msgs::TransformStamped transformStamped;
+
+    for (size_t i = 0; i < count; i++) //전체 몇대 인지를 어떻게 받을것이가 param?
+    {
+        try
+        {
+            transformStamped = tfBuffer.lookupTransform("camila@_base_link", "camila@_base_link",
+                                                        ros::Time(0));
+            //벡터로 바꿔서 전역 벡터 배열에 넣기
+        }
+        catch (tf2::TransformException &ex)
+        {
+            ROS_WARN("%s", ex.what());
+            ros::Duration(1.0).sleep();
+            continue;
+        }
+    }
+}
+
+Vector3 seek(){}
+Vector3 seperate(){}
+
